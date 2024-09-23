6798a8bc40c6510a4fbc7c9db4cf76bfd2b4cd97
Kobi Hari <kobi2294@yahoo.com>
2024-09-23 18:36:55 +0300

    ex4 completed

--- ex04-angular-components/solution/src/app/app.component.ts
+++ ex04-angular-components/solution/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
+import { POSTS } from './data/posts.mock';
+import { Post } from './models/post.model';
+import { addPost, deletePost } from './logic/post-helpers';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [PostListComponent, CreatePostComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'solution';
+  readonly posts = signal(POSTS);
+
+  addPost(post: Post) {
+    this.posts.update(posts => addPost(posts, post));
+  }
+
+  deletePost(postId: number) {
+    this.posts.update(posts => deletePost(posts, postId));
+  }
+
 }
--- ex04-angular-components/solution/src/app/logic/post-helpers.ts
+++ ex04-angular-components/solution/src/app/logic/post-helpers.ts
@@ -1,7 +1,20 @@
+import { resolveForwardRef } from "@angular/core";
+import { Post } from "../models/post.model";
+
 export function wordsCount(text: string): number {
   return text
     .trim()
     .split(' ')
     .filter(word => word.length > 0)
     .length;
+}
+
+export function addPost(posts: Post[], newPost: Post): Post[] {
+  const res = [...posts, newPost];
+  return res;
+}
+
+export function deletePost(posts: Post[], postId: number): Post[] {
+  const res = posts.filter(post => post.id !== postId);
+  return res;
 }
