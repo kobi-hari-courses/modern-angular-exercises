74eaf93c7574fb397c6d41cba3470db5d1d6e890
Kobi Hari <kobi2294@yahoo.com>
2024-09-23 19:04:32 +0300

    ex05 step 02

--- ex05-angular-components/solution/src/app/app.component.ts
+++ ex05-angular-components/solution/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ALL_QUESTIONS } from './data/all-questions';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'solution';
+  readonly questions = signal(ALL_QUESTIONS);
+  readonly answers = signal<number[]>([]);
+
+  
 }
