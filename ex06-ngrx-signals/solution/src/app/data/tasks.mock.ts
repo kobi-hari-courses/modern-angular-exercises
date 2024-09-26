b0a6f00ac4a61242432d9f0ba5d0cd8c05443c99
Kobi Hari <kobi2294@yahoo.com>
2024-09-26 18:58:11 +0300

    ex06 step 02 - Created model and mock data

---
+++ ex06-ngrx-signals/solution/src/app/data/tasks.mock.ts
@@ -0,0 +1,14 @@
+import { Task } from "../models/task.model";
+
+export const MOCK_TASKS: Task[] = [
+    { id: 1, title: "Do the dishes", completed: false },
+    { id: 2, title: "Do the laundry", completed: true },
+    { id: 3, title: "Walk the dog", completed: false },
+    { id: 4, title: "Take out the trash", completed: true },
+    { id: 5, title: "Vacuum the floor", completed: false }, 
+    { id: 6, title: "Mow the lawn", completed: true }, 
+    { id: 7, title: "Do the grocery shopping", completed: false },
+    { id: 8, title: "Wash the car", completed: true },
+    { id: 9, title: "Clean the bathroom", completed: false },
+    { id: 10, title: "Change the bed sheets", completed: true }
+]
