b10cabb51f0d15ca306b4df373f94083dc0d29c1
Kobi Hari <kobi2294@yahoo.com>
2024-09-26 19:08:06 +0300

    ex06 step 05 - Added computed signals to the signal store

--- ex06-ngrx-signals/solution/src/app/store/tasks.store.ts
+++ ex06-ngrx-signals/solution/src/app/store/tasks.store.ts
@@ -1,6 +1,12 @@
-import { signalStore, withState } from "@ngrx/signals";
+import { signalStore, withComputed, withState } from "@ngrx/signals";
 import { initialTasksSlice } from "./tasks.slice";
+import { computed } from "@angular/core";
 
 export const TasksStore = signalStore(
-    withState(initialTasksSlice)
+    withState(initialTasksSlice), 
+    withComputed(store => ({
+        totalTasksCount: computed(() => store.tasks().length), 
+        completedTasksCount: computed(() => store.tasks().filter(t => t.completed).length), 
+        pendingTasksCount: computed(() => store.tasks().filter(t => !t.completed).length)
+    }))
 );
