65b39ea6727e88ac7bb9e4715bdc83909f4c3887
Kobi Hari <kobi2294@yahoo.com>
2024-09-30 19:20:10 +0300

    ex07 step 06 - Busy indicator

--- ex07-ngrx-rxjs-interop/solution/src/app/app.component.ts
+++ ex07-ngrx-rxjs-interop/solution/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TasksListComponent } from "./components/tasks-list/tasks-list.component";
+import { TasksStore } from './store/tasks.store';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,6 @@ import { TasksListComponent } from "./components/tasks-list/tasks-list.component
   styleUrl: './app.component.scss', 
 })
 export class AppComponent {
+  readonly store = inject(TasksStore);
+  readonly isBusy = computed(() => this.store.loadState() === 'loading');
 }
