e87315dce0747661c0b2241e377d1fbfc6ff3456
Kobi Hari <kobi2294@yahoo.com>
2024-09-30 19:02:41 +0300

    ex07 step 5

--- ex07-ngrx-rxjs-interop/solution/src/app/store/tasks.store.ts
+++ ex07-ngrx-rxjs-interop/solution/src/app/store/tasks.store.ts
@@ -1,7 +1,11 @@
 import { getState, patchState, signalStore, withComputed, withHooks, withMethods, withState } from "@ngrx/signals";
 import { initialTasksSlice } from "./tasks.slice";
-import { computed, effect } from "@angular/core";
-import { addTask, getFilteredTasks, removeTask, toggleTaskCompletion } from "./tasks.helpers";
+import { computed, effect, inject } from "@angular/core";
+import { addTask, getFilteredTasks, removeTask, setIdle, setLoading, toggleTaskCompletion } from "./tasks.helpers";
+import { readableStreamLikeToAsyncGenerator } from "rxjs/internal/util/isReadableStreamLike";
+import { DataService } from "../services/data.service";
+import { rxMethod } from "@ngrx/signals/rxjs-interop";
+import { exhaustMap, map, tap } from "rxjs";
 
 export const TasksStore = signalStore(
     {providedIn: 'root'},
@@ -12,15 +16,23 @@ export const TasksStore = signalStore(
         pendingTasksCount: computed(() => store.tasks().filter(t => !t.completed).length),
         filteredTasks: computed(() => getFilteredTasks(store.filter(), store.tasks()))
     })), 
-    withMethods(store => ({
-        addTask: (title: string) => {},
-        toggleTaskCompletion: (id: number) => {},
-        removeTask: (id: number) => {},
-        setFilter: (filter: string) =>
-            patchState(store, state => ({...state, filter}))
-    })), 
+    withMethods(store => { 
+        const dataService = inject(DataService);
+        return {
+            _reload: rxMethod<void>(trigger$ => trigger$.pipe(
+                tap(() => patchState(store, setLoading())), 
+                exhaustMap(() => dataService.getAllTasks()), 
+                tap(tasks => patchState(store, ({tasks}), setIdle()))
+            )),
+            addTask: (title: string) => {},
+            toggleTaskCompletion: (id: number) => {},
+            removeTask: (id: number) => {},
+            setFilter: (filter: string) =>
+                patchState(store, state => ({...state, filter}))
+        }}), 
     withHooks(store => ({
         onInit() {
+            store._reload();
         }
     }))
 );
